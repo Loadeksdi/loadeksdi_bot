22a106405b2d238f31f6ed61619452a226f317af
Loadeksdi <nicolas.load@gmail.com>
2021-04-10 22:30:10 +0200

    Dev: HTTP -> WS working

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require('dotenv').config({path: __dirname + '/.env'});
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const Koa = require('koa');
 const cors = require('@koa/cors');
 const koaBody = require('koa-body');
 const ratelimit = require('koa-ratelimit');
+
 const app = new Koa();
 const db = new Map();
+const sockets = new Map();
 
 app.use(cors());
 app.use(koaBody());
@@ -24,6 +27,21 @@ app.use(ratelimit({
     disableHeader: false
 }));
 
+const server = require('http').createServer(app.callback());
+const whitelist = ['http://localhost:63342', 'http://loadeksdi.github.io'];
+const io = require('socket.io')(server, {
+    cors:
+        {
+            origin: function (origin, callback) {
+                if (whitelist.indexOf(origin) !== -1) {
+                    callback(null, true)
+                } else {
+                    callback(new Error('Not allowed by CORS'))
+                }
+            }
+        }
+});
+server.listen(3000);
 let user;
 
 client.on('ready', async () => {
@@ -31,19 +49,24 @@ client.on('ready', async () => {
     user = await client.users.fetch(process.env.USER_ID);
 });
 
-client.on('message', msg => {
-    if (msg.author === user){
-        console.log(msg);
+io.on('connection', (socket) => {
+    console.log("Connected");
+    socket.on('message', async (msg) => {
+        const discordMessage = await user.send(`**${msg.author}** said: ${msg.text}`);
+        sockets.set(discordMessage.id, socket);
+    });
+});
+
+client.on('message', async msg => {
+    if (msg.author.bot) {
+        return;
+    }
+    if (msg.author === user && msg.reference) {
+        const messageContent = {
+            text: msg.content
+        };
+        sockets.get(msg.reference.messageID).emit('message', messageContent);
     }
 });
 
-app.use(async ctx => {
-    if (ctx.request.is('application/json')) {
-        ctx.status = 200;
-        const msg = ctx.request.body.message;
-        user.send(`*${JSON.stringify(msg.author)}* said: ${JSON.stringify(msg.text)}`);
-    }
-});
-
-app.listen(3000);
 client.login(process.env.BOT_TOKEN);
